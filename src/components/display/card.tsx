2d1f839a5b68e6caea572538b7f9e28d0e4d92c6
useradiw <adiwsnu@gmail.com>
2024-01-19 16:39:50 +0700

    add notion API and react query

--- src/components/display/card.tsx
+++ src/components/display/card.tsx
@@ -9,28 +9,33 @@ interface Props {
 
 const Card = (props: Props) => {
     const { imageSrc, title, link } = props;
+    const newLink = link ?? "localhost:3000";
     return (
         <>
-            <div className="flex flex-row hover:bg-slate-50/5 hover:rounded-md hover:border hover:border-slate-50/10 hover:cursor-pointer w-96 h-40 gap-4">
-                <div className="py-4">
-                    {imageSrc ? 
-                        <Image 
-                            src={imageSrc}
-                            alt="Hero Image"
-                            width={400}
-                            height={200}
-                            style={{
-                                aspectRatio: 16/9
-                            }}
-                        />
-                    :
-                        <div className=" w-52 bg-slate-600 rounded-md h-full text-center">No Image</div>
-                    }
+            <Link href={newLink}>
+                <div className="flex flex-row hover:bg-slate-50/5 hover:rounded-md hover:border hover:border-slate-50/10 hover:cursor-pointer w-96 h-40 gap-4">
+                    <div className="py-4">
+                        {imageSrc ? 
+                            <Image 
+                                src={imageSrc}
+                                alt="Hero Image"
+                                width={400}
+                                height={200}
+                                style={{
+                                    aspectRatio: 16/9
+                                }}
+                            />
+                        :
+                            <div className="w-52 bg-slate-600 rounded-md h-full text-center">No Image</div>
+                        }
+                    </div>
+                    <div className="h-full w-full flex flex-row items-center">
+                        <div>
+                            {title ?? "No Title"}
+                        </div>
+                    </div>
                 </div>
-                <div className="h-full w-full">
-                    {title ?? "No Title"}
-                </div>
-            </div>
+            </Link>
         </>
     )
 };
---
+++ src/type.d.ts
@@ -0,0 +1,82 @@
+type fetchData = {
+  object: string;
+  id: string;
+  created_time: string;
+  last_edited_time: string;
+  created_by: object;
+  last_edited_by: object;
+  cover: {
+    type: string;
+    external: {
+      url: string;
+    };
+  };
+  icon: object;
+  parent: object;
+  archived: boolean;
+  properties: properties;
+  url: string;
+  public_url: object;
+  request_id: string;
+};
+
+type annotations = {
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+};
+
+type richText = {
+  id: string;
+  type: string;
+  rich_text: {
+    type: string;
+    text: {
+      content: string;
+      link: object;
+    };
+    annotations: annotations;
+    plain_text: string;
+    href: object;
+  }[];
+};
+
+type properties = {
+  Tags: {
+    id: string;
+    type: string;
+    multi_select: {
+      id: string;
+      name: string;
+      color: string;
+    }[];
+  };
+  Highlight: richText;
+  Title: {
+    id: string;
+    type: string;
+    title: {
+      type: string;
+      text: {
+        content: string;
+        link: string;
+      };
+      annotations: annotations;
+      plain_text: string;
+      href: string;
+    }[];
+  };
+  Author: richText;
+  Created: {
+    id: string;
+    type: string;
+    date: {
+      start: string;
+      end: string;
+      time_zone: string;
+    };
+  };
+};
