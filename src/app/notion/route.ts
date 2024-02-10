955afb1a4e916785786ab180874e4e8ae3a09d8f
useradiw <adiwsnu@gmail.com>
2024-02-10 13:36:04 +0700

    add cache no-store to notion helper

--- src/app/notion/route.ts
+++ src/app/notion/route.ts
@@ -4,16 +4,23 @@ import { Client } from "@notionhq/client";
 const secret = process.env.NOTION_SECRET;
 const dbid = process.env.NOTION_DATABASE_ID;
 
-export async function GET( req: Request ) {
-    if (!secret || !dbid) return NextResponse.json({message: "Some Environment Variable are missing."}, {status: 500})
+export async function GET(req: Request) {
+  if (!secret || !dbid)
+    return NextResponse.json(
+      { message: "Some Environment Variable are missing." },
+      { status: 500 }
+    );
 
-    try {
-        const notion = new Client({auth: secret})
-        const query = await notion.databases.query({
-            database_id: dbid,
-        })
-        return NextResponse.json({body: query.results}, {status: 200})
-    } catch (error) {
-        return NextResponse.json({message: "Something went wrong.", error: error}, {status: 400})
-    }
-};
+  try {
+    const notion = new Client({ auth: secret });
+    const query = await notion.databases.query({
+      database_id: dbid,
+    });
+    return NextResponse.json({ body: query.results }, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: "Something went wrong.", error: error },
+      { status: 400 }
+    );
+  }
+}
--- src/lib/notion-helper.tsx
+++ src/lib/notion-helper.tsx
@@ -1,10 +1,10 @@
 export const getPosts = async () => {
-    const res = await fetch("/notion");
+    const res = await fetch("/notion", { cache: "no-store"});
     return await res.json();
 };
 
 export const getPost = async ({ queryKey }: any) => {
     const [ _, pageid ] = queryKey;
-    const res = await fetch(`/notion/pages/${pageid}`);
+    const res = await fetch(`/notion/pages/${pageid}`, { cache: "no-store"});
     return await res.json();
 };
